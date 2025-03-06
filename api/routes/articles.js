4109ec8d11ccc5a8145896c4f5f89347f9e9c0d1
emthaa <dominickzdun@outlook.com>
2025-03-05 21:39:12 -0600

    Added comments

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -25,4 +25,12 @@ router.post(
   articlesController.createPost
 );
 
+router.get("/:postID/comments", articlesController.getComments);
+
+router.post(
+  "/:postID/comments",
+  passport.authenticate("jwt", { session: false }),
+  articlesController.createComment
+);
+
 module.exports = router;
